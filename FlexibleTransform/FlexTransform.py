8aec2ddbd2ef9b2df0955d854fa8e6174a1cf24e
ahoying <andrew.hoying@nrel.gov>
2014-12-23 09:35:27 -0700

    Initial GIT commit

--- FlexibleTransform/FlexTransform.py
+++ FlexibleTransform/FlexTransform.py
@@ -4,7 +4,7 @@ Created on Jul 27, 2014
 @author: ahoying
 '''
 
-from Configuration import Config
+from Configuration import Config  # @UnresolvedImport
 
 # TODO: Implement logging framework
 # TODO: Document in Sphinx compatible format
